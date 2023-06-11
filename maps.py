f693e04b1c6426617730c3f84935fffd0d44e229
Phanendra Sai Ram <125650236+heisenberg3376@users.noreply.github.com>
2023-06-11 09:04:52 +0530

    added weather icons

--- maps.py
+++ maps.py
@@ -3,6 +3,8 @@ import folium
 import streamlit as st
 from streamlit_folium import st_folium
 import requests
+from io import BytesIO
+from PIL import Image
 
 
 st.header("Select an Indian City and I will show it's Properties\n")
@@ -20,19 +22,51 @@ weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q=
 #st.write(weather_data.json())
 
 climate = (weather_data.json().get('weather','')[0].get('main',''))
+cli_icon = (weather_data.json().get('weather','')[0].get('icon',''))
 desc = (weather_data.json().get('weather','')[0].get('description',''))
 lat = (weather_data.json().get('coord','').get('lat',''))
 lon = (weather_data.json().get('coord','').get('lon',''))
+wind_speed = (weather_data.json().get('wind','').get('speed',''))
 temp = (weather_data.json().get('main','').get('temp',''))
 hum = (weather_data.json().get('main','').get('humidity',''))
 name = (weather_data.json().get('name',''))
+  
 
-data = pd.DataFrame({'city':name,'Weather':[climate],'description':[desc],'latitude':[lat],'longitude':[lon],'temperature':str("{0:.3f}".format((temp - 32)*(5/9)))+' °C','humidity':str(hum)+'%'},index=['data'])
+data = pd.DataFrame({'city':name,'Weather':[climate],'description':[desc],'latitude':[lat],'longitude':[lon],
+                     'temperature':str("{0:.3f}".format((temp - 32)*(5/9)))+' °C','humidity':str(hum)+'%'},index=['data'])
 st.dataframe(data,width=900,use_container_width=True)
+icon_url = f'https://openweathermap.org/img/w/{cli_icon}.png'
+icon_response = requests.get(icon_url)
+icon_content = icon_response.content
+icon_bytesIO = BytesIO(icon_content)
+st.write('''## Weather Condition ,temperature and humidity:''')
+l,m,r = st.columns(3,gap='large')
+with l:
+    st.header('Weather Condition :')
+    st.header('Temperature(°C)')
+    st.header('Wind Speed(m/s)')
+    st.header('Humidity(%)')
+with m:
+    st.image(icon_bytesIO,width=100)
+    st.image('Temperature-Icon.png',width=50)
+    st.image('wind_speed.png',width=80)
+    
+    st.image('hu.png',width=120)
+with r:
+    st.header(f'{desc}')
+    st.write('   ')
+    st.write('   ')
+    
+    st.header(str("{0:.2f}".format((temp - 32)*(5/9)))+' °C')
+    st.write('')
+    st.header(f'{wind_speed} m/s')
+    st.write('  ')
+    st.write('  ')
+    st.header(f'{hum}%')
+
 m = folium.Map(location=[df.iloc[[0]].lat,df.iloc[[0]].lon],zoom_start=12)
 folium.Marker([df.iloc[[0]].lat,df.iloc[[0]].lon],popup =df.iloc[[0]].City,tooltip=df.iloc[[0]].City).add_to(m)
 st_data = st_folium(m, width=775)
-
 
 
 
